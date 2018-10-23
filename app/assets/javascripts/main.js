74ce6fc2b2e9d2345c057f8772a9402fcfdffe19
john <jiama843@gmail.com>
2018-10-23 01:24:55 -0400

    <Finished landing page>

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -8,8 +8,37 @@ $(document).on("turbolinks:load", function(){
 
     // Load introduction (Hi, I'm John)
     $('.oneshot_icon').slideDown(1618, function(){
-      $('#introduction').fadeIn(1618);
+      $('#introduction').fadeIn(1618, function(){
+        $('#app_body').fadeIn(1618);
+      });
     });
 
+    // Fade out elements upon scroll
+    $(window).scroll(function(){
+      $(".container").each(function(){
+        //console.log($(this));
+        //console.log($(window).scrollTop());
+        //console.log($(window).height());
+        $(this).css("opacity", 1 - ($(this).offset().top - $(window).scrollTop()) / $(window).height()) * 1.2;
+      });
+    });
+
+    // Redirect to link when project card is clicked
+    $(".card").click(function() {
+      window.location = $(this).find("a").attr("href"); 
+      return false;
+    });
+
+    /* Change 
+    $(".card").each(function(){
+      $(this).hover(
+        function(){
+          $(this).addClass( "hover" );
+        },
+        function(){
+          $(this).removeClass( "hover" );
+        }
+      );
+    });*/
   }
 });
